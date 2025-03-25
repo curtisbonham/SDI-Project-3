16a13b5ec1849e72ba2d328dcd10edaf37d223f9
curtisbonham <curtis.bonham1@gmail.com>
2025-03-25 13:57:26 -0600

    finished all but css on courses table

--- server/seeds/03_certs.js
+++ server/seeds/03_certs.js
@@ -8,6 +8,10 @@ exports.seed = async function(knex) {
   await knex('certifications').insert([
     {id: 1, position: 'Purveyor_of_Fine_Goods'},
     {id: 2, position: 'Linguist'},
-    {id: 3, position: 'Cook'}
+    {id: 3, position: 'Cook'},
+    {id: 4, position: 'Deckhand'},
+    {id: 5, position: 'Space_Surgeon'},
+    {id: 6, position: 'Crew_Commander'},
+    {id: 7, position: 'Space_Engineer'}
   ]);
 };
